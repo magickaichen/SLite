1bfe7e6ace6b6f6fe1b51acab3c465587935a914
Mike Xiao <xiaokaic@cse.msu.edu>
2019-04-03 20:42:46 -0500

    direct messages panel

---
+++ src/components/SidePanel/DirectMessages.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import { Menu, Icon } from "semantic-ui-react";
+import firebase from "../../firebase";
+
+class DirectMessages extends Component {
+  state = {
+    user: this.props.currentUser,
+    users: [],
+    usersRef: firebase.database().ref("users"),
+    connectedRef: firebase.database().ref(".info/connected"),
+    presenceRef: firebase.database().ref("presence")
+  };
+
+  componentDidMount() {
+    if (this.state.user) {
+      this.addListener(this.state.user.uid);
+    }
+  }
+
+  addStatusToUser = (uid, connected = true) => {
+    const updatedUsers = this.state.users.reduce((acc, user) => {
+      if (user.uid === uid) {
+        user["status"] = `${connected ? "online" : "offline"}`;
+      }
+      return acc.concat(user);
+    }, []);
+    this.setState({ users: updatedUsers });
+  };
+
+  addListener = currentUserUid => {
+    let loadedUsers = [];
+    this.state.usersRef.on("child_added", snap => {
+      if (currentUserUid !== snap.key) {
+        let user = snap.val();
+        user["uid"] = snap.key;
+        user["status"] = "offline";
+        loadedUsers.push(user);
+        this.setState({ users: loadedUsers });
+      }
+    });
+
+    this.state.connectedRef.on("value", snap => {
+      if (snap.val() === true) {
+        const ref = this.state.presenceRef.child(currentUserUid);
+        ref.set(true);
+        ref.onDisconnect().remove(err => {
+          if (err !== null) {
+            console.error(err);
+          }
+        });
+      }
+    });
+
+    this.state.presenceRef.on("child_added", snap => {
+      if (currentUserUid !== snap.key) {
+        this.addStatusToUser(snap.key);
+      }
+    });
+
+    this.state.presenceRef.on("child_removed", snap => {
+      if (currentUserUid !== snap.key) {
+        this.addStatusToUser(snap.key, false);
+      }
+    });
+  };
+
+  isUserOnline = user => user.status === "online";
+
+  render() {
+    const { users } = this.state;
+    return (
+      <Menu.Menu className="menu">
+        <Menu.Item>
+          <span>
+            <Icon name="mail" /> DIRECT MESSAGES
+          </span>{" "}
+          ({users.length})
+        </Menu.Item>
+        {/* Users to send direct msgs */}
+        {users.map(user => (
+          <Menu.Item
+            key={user.uid}
+            onClick={() => console.log(user)}
+            style={{ opacity: 0.7, fontStyle: "italic" }}
+          >
+            <Icon
+              name="circle"
+              size="small"
+              color={this.isUserOnline(user) ? "green" : "red"}
+            />
+            @ {user.name}
+          </Menu.Item>
+        ))}
+      </Menu.Menu>
+    );
+  }
+}
+
+export default DirectMessages;
--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
 import UserPanel from './UserPanel';
 import Channels from './Channels';
+import DirectMessages from './DirectMessages';
 
 class SidePanel extends Component {
   state = {  }
@@ -17,6 +18,7 @@ class SidePanel extends Component {
         style={{ background: '#4c3c4c', fontSize: '1.2rem'}}>
         <UserPanel currentUser={currentUser} />
         <Channels currentUser={currentUser} />
+        <DirectMessages currentUser={currentUser} />
       </Menu>
     );
   }
